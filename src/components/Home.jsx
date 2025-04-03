a8b155e048d90405a29c8febe6c33da7743098e1
Anshul <anshuls26@gmail.com>
2025-04-03 13:59:29 +0530

    update format for image upload and enhance image

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ImagePreview from "./ImagePreview";
+import ImageUpload from "./ImageUpload";
+
+const Home = () => {
+  return (
+    <div>
+      <ImageUpload />
+      <ImagePreview />
+    </div>
+  );
+};
+
+export default Home;
