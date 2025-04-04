05a3bc5593622cb1916b1c39912beae002ccec4a
Anshul <anshuls26@gmail.com>
2025-04-04 12:21:21 +0530

    update image upload behaviour

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import ImagePreview from "./ImagePreview";
 import ImageUpload from "./ImageUpload";
 
 const Home = () => {
+  const [upload, setUpload] = useState(null);
+  const [enhancedImage, setEnhancedImage] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const uploadImageHandler = (file) =>{
+    setUpload(URL.createObjectURL(file));
+    setLoading(true)
+
+    // call the API to enhance the image
+    
+  }
+
   return (
-    <div>
-      <ImageUpload />
-      <ImagePreview />
-    </div>
+    <>
+      <ImageUpload uploadImageHandler={uploadImageHandler} />
+      <ImagePreview
+        loading={loading}
+        uploaded={upload}
+        enhanced={enhancedImage}
+      />
+    </>
   );
 };
 
